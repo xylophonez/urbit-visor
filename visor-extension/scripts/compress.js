8d2110147e46407af770090917e4f863080cdc61
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-10-21 12:33:49 +0200

    Fixed compress script

--- visor-extension/scripts/compress.js
+++ visor-extension/scripts/compress.js
@@ -1,13 +1,13 @@
 // add package-specific dependencies
 module.paths.unshift(`${process.cwd()}/node_modules`);
 
-const fs = require('fs');
-const path = require('path');
-const ChromeExtension = require('crx');
-const argv = require('minimist')(process.argv.slice(2));
+const fs = require("fs");
+const path = require("path");
+const ChromeExtension = require("crx");
+const argv = require("minimist")(process.argv.slice(2));
 /* eslint import/no-unresolved: 0 */
 
-const name = require(path.join(process.cwd(), '/build/manifest.json')).name;
+const name = require(path.join(process.cwd(), "/dist/manifest.json")).name;
 
 function readKeyFromFile(keyPath) {
   if (!fs.existsSync(keyPath)) {
@@ -18,30 +18,30 @@ function readKeyFromFile(keyPath) {
 
 function getPrivateKey() {
   if (argv.key == null) return null;
-  if (argv.key.startsWith('./')) {
+  if (argv.key.startsWith("./")) {
     return readKeyFromFile(argv.key);
   }
-  return Buffer.from(argv.key, 'utf-8');
+  return Buffer.from(argv.key, "utf-8");
 }
 
 const privateKey = getPrivateKey();
-const zipOnly = argv['zip-only'];
+const zipOnly = argv["zip-only"];
 const isCrx = !zipOnly;
 
 if (!argv.codebase) {
-  console.error('Missing codebase param.');
+  console.error("Missing codebase param.");
   process.exit();
 }
 
 const crx = new ChromeExtension({
-  appId: argv['app-id'],
+  appId: argv["app-id"],
   codebase: argv.codebase,
   version: 3,
   privateKey,
 });
 
 async function compress(isCrxBuild) {
-  await crx.load(path.join(process.cwd(), '/build'));
+  await crx.load(path.join(process.cwd(), "/dist"));
   const archiveBuffer = await crx.loadContents();
   fs.writeFileSync(`${name}.zip`, archiveBuffer);
 
@@ -62,4 +62,4 @@ async function compress(isCrxBuild) {
   }
 }
 
-compress(isCrx).catch(err => console.error(err));
+compress(isCrx).catch((err) => console.error(err));
