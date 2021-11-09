856c03526768e8b25b46da326e481700a087f158
eugenia romero <eromero+github@atixlabs.com>
2021-11-08 22:12:12 -0300

    feat: new styles to setup pages

--- visor-extension/src/components/adding/Form.tsx
+++ visor-extension/src/components/adding/Form.tsx
@@ -5,39 +5,49 @@ import { fetchShipname, scrapeShipname } from "../../urbit";
 import { Messaging } from "@dcspark/uv-core";
 import "./adding.css";
 import { motion } from "framer-motion";
-
+import icon from "../../icons/plus-icon.svg";
 
 interface AddShipFormProps {
-  url: string,
-  code: string,
-  setUrl: (v: string) => void,
-  setCode: (v: string) => void,
-  setShipName: (shipName: string) => void,
+  url: string;
+  code: string;
+  setUrl: (v: string) => void;
+  setCode: (v: string) => void;
+  setShipName: (shipName: string) => void;
 }
 
-export default function AddShipForm({ url, code, setUrl, setCode, setShipName }: AddShipFormProps) {
+export default function AddShipForm({
+  url,
+  code,
+  setUrl,
+  setCode,
+  setShipName,
+}: AddShipFormProps) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-
   async function postLogin(url: string, code: string): Promise<void> {
-    const controller = new AbortController()
-    setTimeout(() => { controller.abort() }, 5000)
-    fetch(url.replace(/\/$/g, '') + "/~/login", {
+    const controller = new AbortController();
+    setTimeout(() => {
+      controller.abort();
+    }, 5000);
+    fetch(url.replace(/\/$/g, "") + "/~/login", {
       body: `password=${code}`,
       method: "POST",
       credentials: "include",
       redirect: "follow",
-      signal: controller.signal
+      signal: controller.signal,
     })
-      .then(async res => {
+      .then(async (res) => {
         switch (res.status) {
           case 204:
-            Messaging.sendToBackground({ action: "cache_form_url", data: { url: "" } });
+            Messaging.sendToBackground({
+              action: "cache_form_url",
+              data: { url: "" },
+            });
             setLoading(false);
             scrapeShipname(url)
-              .then(shipName => setShipName(shipName))
-              .catch(err => setError("Your ship needs an OS update"))
+              .then((shipName) => setShipName(shipName))
+              .catch((err) => setError("Your ship needs an OS update"));
             break;
           case 400:
             setError("Invalid +code.\nCould not connect to ship.");
@@ -47,75 +57,94 @@ export default function AddShipForm({ url, code, setUrl, setCode, setShipName }:
             setError("Invalid URL.\nCould not connect to ship.");
             setLoading(false);
             break;
-        };
+        }
       })
-      .catch(err => {
-        console.log(err, "err")
-        setError("Could not connect")
+      .catch((err) => {
+        console.log(err, "err");
+        setError("Could not connect");
         setLoading(false);
-      })
+      });
   }
 
-
-  const spinner = <Spinner width="24" height="24" innerColor="white" outerColor="black" />
-
+  const spinner = (
+    <Spinner width="24" height="24" innerColor="white" outerColor="black" />
+  );
 
   const onChangeURL = (e: React.FormEvent<HTMLInputElement>) => {
     setUrl(e.currentTarget.value);
-    Messaging.sendToBackground({ action: "cache_form_url", data: { url: e.currentTarget.value } });
-  }
-  const onChangeCode = (e: React.FormEvent<HTMLInputElement>) => setCode(e.currentTarget.value)
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    Messaging.sendToBackground({
+      action: "cache_form_url",
+      data: { url: e.currentTarget.value },
+    });
+  };
+  const onChangeCode = (e: React.FormEvent<HTMLInputElement>) =>
+    setCode(e.currentTarget.value);
+  const onSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     e.preventDefault();
-    setError("")
+    setError("");
     setLoading(true);
     postLogin(url, code);
-  }
+  };
 
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="full-size"
+      className="padding flex-grow-wrapper"
     >
-      <form className="form padding flex-grow-wrapper" onSubmit={onSubmit}>
-        <p className="form-prompt">Input Your Urbit Ship Credentials</p>
+      <div>
+        <div className="container-progress">
+          <div className="progress-bar" style={{ width: "66%" }} />
+          <span className="progress-step">Step 2/3</span>
+        </div>
+        <img src={icon} className="icon" />
+        <h2 className="mid-title">Input Your Urbit Ship Credentials</h2>
+      </div>
+      <form className="form" onSubmit={onSubmit}>
         <div className="inputs flex-grow">
-          <label htmlFor="shipURL">
+          <label className="label-input" htmlFor="shipURL">
             URL
-            <input
-              type="text"
-              name='shipURL'
-              id='loginFormShipURL'
-              value={url}
-              placeholder='http://localhost'
-              onChange={onChangeURL}
-              required
-            />
           </label>
-          <label htmlFor="shipCode">
+          <input
+            type="text"
+            name="shipURL"
+            id="loginFormShipURL"
+            value={url}
+            placeholder="http://localhost"
+            onChange={onChangeURL}
+            required
+          />
+          <label className="label-input" htmlFor="shipCode">
             +code
-            <input
-              type="password"
-              name='shipCode'
-              id='loginFormShipCode'
-              value={code}
-              placeholder='sampel-sampel-sampel-sampel'
-              onChange={onChangeCode}
-              maxLength={27}
-              required
-            />
           </label>
+          <input
+            type="password"
+            name="shipCode"
+            id="loginFormShipCode"
+            value={code}
+            placeholder="sampel-sampel-sampel-sampel"
+            onChange={onChangeCode}
+            maxLength={27}
+            required
+          />
         </div>
         <div className="errorMessage">
           {loading && <div className="spinner">{spinner}</div>}
-          {error.split("\n").map((p) => <p key={p}>{p}</p>)}
+          {error.split("\n").map((p) => (
+            <p key={p}>{p}</p>
+          ))}
         </div>
-        <button disabled={code.length < 27} className="single-button" type='submit'>
+        <button
+          disabled={code.length < 27}
+          className="linear-button"
+          type="submit"
+        >
           Add Ship
         </button>
       </form>
     </motion.div>
-  )
+  );
 }
