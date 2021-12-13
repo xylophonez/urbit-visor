5b0bcb3e5abaff6c7abb91f8c7268a127afbdccd
mirtyl-wacdec <ycl2@protonmail.com>
2021-12-13 11:12:42 +0700

    require function implemented

--- uv-core/messaging.ts
+++ uv-core/messaging.ts
@@ -1,23 +1,6 @@
-import { UrbitVisorRequest, UrbitVisorEvent, UrbitVisorResponse, UrbitVisorState, UrbitVisorInternalComms } from "./types";
+import { UrbitVisorRequest, UrbitVisorEvent, UrbitVisorResponse } from "./types";
 
 const Messaging = {
-    sendToPopup: async function (message: { state: UrbitVisorState }): Promise<void> {
-        return new Promise((res, rej) =>
-            chrome.runtime.sendMessage(message, (response) => res(response))
-        );
-    },
-    sendToBackground: async function (request: UrbitVisorInternalComms): Promise<any> {
-        return new Promise((res, rej) =>
-            chrome.runtime.sendMessage({
-                ...request, app: "urbit-visor-internal",
-            }, (response) => res(response))
-        );
-    },
-    relayToBackground: async function (request: UrbitVisorRequest): Promise<UrbitVisorResponse> {
-        return new Promise((res, rej) =>
-            chrome.runtime.sendMessage(request, (response) => res(response))
-        );
-    },
     pushEvent: function (event: UrbitVisorEvent, recipients: Set<any>) {
       for (let id of recipients) {
           if (typeof id == "number") chrome.tabs.sendMessage(id, { app: "urbitVisorEvent", event: event})
@@ -41,31 +24,6 @@ const Messaging = {
             });
             window.postMessage({ action, data, app, id: requestId }, window.origin);
         });
-    },
-    createProxyController: () => {
-        console.log("Urbit Visor present")
-        //listen to function calls from webpage
-        window.addEventListener('message', async function (e) {
-            const request = e.data;
-            if (request && request.app !== "urbitVisor") return;
-            // relay message to background script
-            request.origin = e.origin;
-            Messaging.relayToBackground(request).then((response: UrbitVisorResponse) => {
-                // relay back responses to webpage
-                window.postMessage({ app: "urbitVisorResponse", id: request.id, status: response?.status, response: response?.response }, window.origin)
-            })
-            return;
-        });
-        // listen to events from the background script
-        chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-            // relay events to webpage
-            if (request.app == "urbitVisorEvent") {
-                window.postMessage(request, window.origin);
-                sendResponse("ok")
-            } else sendResponse("ng")
-            return true
-        }
-        );
     }
 };
 
